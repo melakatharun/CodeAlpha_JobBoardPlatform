ba20d18c7742d14848b57c0a6eb93c5e0769eb7e
melakatharun <201201209+melakatharun@users.noreply.github.com>
2026-09-05 22:39:45 +0530

    Add API homepage

--- config/urls.py
+++ config/urls.py
@@ -1,27 +1,33 @@
+```python
 """
 URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
 
+
+def home(request):
+    return JsonResponse({
+        "message": "Job Board Platform API is running",
+        "status": "success",
+        "endpoints": {
+            "accounts": "/api/accounts/",
+            "jobs": "/api/jobs/",
+            "applications": "/api/applications/",
+            "notifications": "/api/notifications/",
+            "admin": "/admin/",
+        }
+    })
+
+
 urlpatterns = [
+    path("", home, name="home"),
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
     path("api/jobs/", include("jobs.urls")),
     path("api/applications/", include("applications.urls")),
     path("api/notifications/", include("notifications.urls")),
 ]
+```
